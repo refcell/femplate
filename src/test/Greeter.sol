65d49ce1e005f23ef4d416820ac2c3cf87661690
Andreas Bigger <abigger87@gmail.com>
2022-01-14 13:13:44 -0500

    fix dstest import

--- src/test/Greeter.sol
+++ src/test/Greeter.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity 0.8.11;
 
-import "ds-test/test.sol";
+import {DSTestPlus} from "solmate/test/utils/DSTestPlus.sol";
 
 import {Greeter} from "../Greeter.sol";
 
-contract GreeterTest is DSTest {
+contract GreeterTest is DSTestPlus {
     Greeter greeter;
 
     function setUp() public {
