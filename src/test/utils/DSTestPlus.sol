f4f147c2becfd4eb366eaae098a6b1916d106b73
Andreas Bigger <abigger87@gmail.com>
2022-01-20 17:26:07 -0500

    modules

--- src/test/utils/DSTestPlus.sol
+++ src/test/utils/DSTestPlus.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity 0.8.11;
 
-import {DSTestPlus as DSTest} from "solmate/test/utils/DSTestPlus.sol";
-import {ERC20} from "solmate/tokens/ERC20.sol";
+import {DSTestPlus as DSTest} from "@solmate/test/utils/DSTestPlus.sol";
+import {ERC20} from "@solmate/tokens/ERC20.sol";
 
 contract DSTestPlus is DSTest {
     function assertERC20Eq(ERC20 erc1, ERC20 erc2) internal {
