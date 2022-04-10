8e55d87c9efca3ceb26fa671a0ab58faa5562bfb
Rohan Sanjay <rohanpsanjay@gmail.com>
2022-04-09 21:39:59 -0700

    change imports and remove console

--- src/Greeter.sol
+++ src/Greeter.sol
@@ -4,17 +4,17 @@ pragma solidity 0.8.12;
 /// @title Greeter
 /// @author Andreas Bigger <andreas@nascent.xyz>
 contract Greeter {
-  string public _gm;
-
-  constructor(string memory newGm) {
-    _gm = newGm;
-  }
-
-  function gm(string memory myGm) public view returns(string memory greeting) {
-    require(keccak256(abi.encodePacked((myGm))) == keccak256(abi.encodePacked((greeting = _gm))), "WRONG_GM");
-  }
-
-  function setGm(string memory newGm) public {
-    _gm = newGm;
-  }
+    string public _gm;
+  
+    constructor(string memory newGm) {
+        _gm = newGm;
+    }
+  
+    function gm(string memory myGm) public view returns(string memory greeting) {
+        require(keccak256(abi.encodePacked((myGm))) == keccak256(abi.encodePacked((greeting = _gm))), "WRONG_GM");
+    }
+  
+    function setGm(string memory newGm) public {
+        _gm = newGm;
+    }
 }
--- src/test/Greeter.t.sol
+++ src/test/Greeter.t.sol
@@ -3,18 +3,19 @@ pragma solidity 0.8.12;
 
 import {Greeter} from "../Greeter.sol";
 import {DSTestPlus} from "./utils/DSTestPlus.sol";
+import "forge-std/console.sol";
 
 contract GreeterTest is DSTestPlus {
     Greeter greeter;
 
     function setUp() public {
-        console.log(unicode"🧪 Testing Greeter.sol...");
+        console.log("Testing Greeter.sol...");
         greeter = new Greeter("gm");
     }
 
     function testSetGm() public {
         greeter.setGm("gm gm");
         greeter.gm("gm gm");
-        console.log(unicode"✅ setGm() test passed");
+        console.log("setGm() test passed");
     }
 }
