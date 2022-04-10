9595a16633725a46046b821fe570524e2565f3b2
Rohan Sanjay <rohanpsanjay@gmail.com>
2022-04-10 00:37:46 -0700

    remove lint ci

--- src/Greeter.sol
+++ src/Greeter.sol
@@ -5,15 +5,15 @@ pragma solidity 0.8.12;
 /// @author Andreas Bigger <andreas@nascent.xyz>
 contract Greeter {
     string public _gm;
-  
+
     constructor(string memory newGm) {
         _gm = newGm;
     }
-  
-    function gm(string memory myGm) public view returns(string memory greeting) {
+
+    function gm(string memory myGm) public view returns (string memory greeting) {
         require(keccak256(abi.encodePacked((myGm))) == keccak256(abi.encodePacked((greeting = _gm))), "WRONG_GM");
     }
-  
+
     function setGm(string memory newGm) public {
         _gm = newGm;
     }
