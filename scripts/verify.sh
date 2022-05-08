cd9a567b6fc835566aa51aae10147d361245f01d
Bruno Barbieri <brunobar79@gmail.com>
2022-05-07 22:09:32 -0400

    Fix verify.sh

--- scripts/verify.sh
+++ scripts/verify.sh
@@ -31,7 +31,7 @@ read -s etherscan
 
 if [ -z "$args" ]
 then
-  forge verify-contract --compiler-version \"$version\" $deployed ./src/${contract}.sol:${contract} $etherscan
+  forge verify-contract --compiler-version "$version" $deployed ./src/${contract}.sol:${contract} $etherscan
 else
-  forge verify-contract --compiler-version \"$version\" $deployed ./src/${contract}.sol:${contract} $etherscan --constructor-args ${args}
+  forge verify-contract --compiler-version "$version" $deployed ./src/${contract}.sol:${contract} $etherscan --constructor-args ${args}
 fi
