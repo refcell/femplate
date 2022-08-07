4dca4e8077e10d9ff547c5874ddb8ac906072463
Andreas Bigger <abigger87@gmail.com>
2022-08-07 15:44:42 -0400

    slither fixes

--- test/Greeter.t.sol
+++ test/Greeter.t.sol
@@ -19,15 +19,18 @@ contract GreeterTest is Test {
     // VM Cheatcodes can be found in ./lib/forge-std/src/Vm.sol
     // Or at https://github.com/foundry-rs/forge-std
     function testSetGm() external {
+        // slither-disable-next-line reentrancy-events
         greeter.setGreeting("gm gm");
 
         // Expect the GMEverybodyGM event to be fired
         vm.expectEmit(true, true, true, true);
         emit GMEverybodyGM();
+        // slither-disable-next-line unused-return
         greeter.gm("gm gm");
 
         // Expect the gm() call to revert
         vm.expectRevert(abi.encodeWithSignature("BadGm()"));
+        // slither-disable-next-line unused-return
         greeter.gm("gm");
 
         // We can read slots directly
